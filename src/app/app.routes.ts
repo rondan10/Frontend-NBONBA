2247031a86a2618f93908ea5e5b82aa6aeecfd79
ronda10 <drnrondan@gmail.com>
2024-10-24 17:26:48 -0500

    ruta componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { ConsultaOfertasComponent } from './features/consulta-ofertas/consulta-ofertas.component';
 
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'busqueda', component: ConsultaOfertasComponent}
 ];
 
 @NgModule({
